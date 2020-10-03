845f5ada302bd2ef6d35d93e2d2e734b6ebf2e83
NikolayS93 <nikolays93@ya.ru>
2020-10-03 23:39:47 +0400

    Split functions

--- includes/template-functions.php
+++ includes/template-functions.php
@@ -6,6 +6,18 @@
  */
 
 /**
+ * Set the content width in pixels, based on the theme's design and stylesheet.
+ *
+ * Priority 0 to make it available to lower priority callbacks.
+ *
+ * @global int $content_width
+ */
+function _s_content_width() {
+    $GLOBALS['content_width'] = apply_filters( '_s_content_width', 640 );
+}
+add_action( 'after_setup_theme', '_s_content_width', 0 );
+
+/**
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
