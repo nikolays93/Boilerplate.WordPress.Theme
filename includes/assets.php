e0651d9853f72b59554ae7c8d423b0f93bdfee7c
Nick Sergeevich <nikolays93@ya.ru>
2021-01-07 00:04:49 +0400

    Remove built in comments enqueue script

--- includes/assets.php
+++ includes/assets.php
@@ -12,9 +12,5 @@ function _s_assets() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), filemtime(get_stylesheet_directory() . '/style.css') );
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', '_s_assets' );
