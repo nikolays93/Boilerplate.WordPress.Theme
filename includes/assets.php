2d5c1dac16bd3d424bea83d7fe0bff9ced1b8ea1
Nick Sergeevich <nikolays93@ya.ru>
2021-01-07 00:04:00 +0400

    Style auto update version

--- includes/assets.php
+++ includes/assets.php
@@ -9,7 +9,7 @@
  * Wp_enqueue_scripts hook.
  */
 function _s_assets() {
-	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( '_s-style', get_stylesheet_uri(), array(), filemtime(get_stylesheet_directory() . '/style.css') );
 
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 
